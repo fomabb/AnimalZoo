55a49f19f5eeef04b8e9036802887d00b3bf8a53
Nikolay <fomabb123@gmail.com>
2021-11-01 22:27:32 +0300

    init fields and add method feed

--- src/by/overone/lesson/Animals.java
+++ src/by/overone/lesson/Animals.java
@@ -9,4 +9,41 @@ public abstract class Animals {
     String feed;
     int feeding;
     int age;
+
+    public void name() {
+        this.name = name;
+    }
+
+    public void age() {
+        this.age = age;
+    }
+
+    public void feed() {
+        this.feed = feed;
+        System.out.println("кормить животное по имени " + name);
+
+        System.out.println("1. Мясом; \n2. Травой; \n3. Сухим кормом.");
+
+        feeding = scanner.nextInt();
+
+        if (feeding >= 4) {
+            System.out.println("У нас только такой корм");
+        }
+
+        switch (feeding) {
+            case 1:
+                String meat = "мясом";
+                return;
+
+            case 2:
+                String tr = "Травой";
+                return;
+
+            case 3:
+                String sx = "Сухим кормом";
+                return;
+
+        }
+        System.out.println();
+    }
 }
