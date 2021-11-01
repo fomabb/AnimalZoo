a532b3aaf024d328a56ff9c03fcddad1b79c1b7c
Nikolay <fomabb123@gmail.com>
2021-11-01 22:42:06 +0300

    create a variable of type scanner, create a variable of typea for it int. add object

--- src/by/overone/lesson/Main.java
+++ src/by/overone/lesson/Main.java
@@ -1,7 +1,13 @@
 package by.overone.lesson;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int feed;
 
+        Zoo zoo = new Zoo();
+        Wolf wolf = new Wolf();
     }
 }
