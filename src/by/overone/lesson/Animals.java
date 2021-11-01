06c77121b0f7e76baea8c6ca00a6b499a8196727
Nikolay <fomabb123@gmail.com>
2021-11-01 22:00:52 +0300

    add class Animals and fields

---
+++ src/by/overone/lesson/Animals.java
@@ -0,0 +1,12 @@
+package by.overone.lesson;
+
+import java.util.Scanner;
+
+public abstract class Animals {
+    Scanner scanner = new Scanner(System.in);
+
+    String name;
+    String feed;
+    int feeding;
+    int age;
+}
---
+++ src/by/overone/lesson/Wolf.java
@@ -0,0 +1,4 @@
+package by.overone.lesson;
+
+public class Wolf {
+}
