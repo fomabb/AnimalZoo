2ee94df44b207b9a36f13706b3c4eea636ba6f01
Nikolay <fomabb123@gmail.com>
2021-11-01 22:32:45 +0300

    add class wolf we take parameters from the parent

--- src/by/overone/lesson/Wolf.java
+++ src/by/overone/lesson/Wolf.java
@@ -1,4 +1,19 @@
 package by.overone.lesson;
 
-public class Wolf {
+public class Wolf extends Animals {
+
+    @Override
+    public void name() {
+        super.name();
+    }
+
+    @Override
+    public void age() {
+        super.age();
+    }
+
+    @Override
+    public void feed() {
+        super.feed();
+    }
 }
