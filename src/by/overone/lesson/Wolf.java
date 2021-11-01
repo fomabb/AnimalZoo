7980e4db25a762b3261f74bb7b8faeeede6fdb38
Nikolay <fomabb123@gmail.com>
2021-11-01 22:35:09 +0300

    we write methods for the child class

--- src/by/overone/lesson/Wolf.java
+++ src/by/overone/lesson/Wolf.java
@@ -5,15 +5,28 @@ public class Wolf extends Animals {
     @Override
     public void name() {
         super.name();
+        name = "Грыка";
+        System.out.println("вот волченок по кличке " + name);
     }
 
     @Override
     public void age() {
         super.age();
+        age = 3;
+        System.out.println("ему исплнилось " + age + " годика\n");
     }
 
     @Override
     public void feed() {
         super.feed();
+        if (feeding == 1) {
+            System.out.println("~~~" + name + " начинает скулить, размахивать хвостиком и просит еще добавки завывая~~~\n");
+        } else if (feeding == 2) {
+            System.out.println("~~~" + name + " рычит и ходит туда обратно по клетке~~~\n");
+        } else if (feeding == 3) {
+            System.out.println("~~~" + name + " ест не хотя и лает~~~\n");
+        } else {
+            System.out.println("~~~Ну же, не бойся, покорми животное~~~\n");
+        }
     }
 }
