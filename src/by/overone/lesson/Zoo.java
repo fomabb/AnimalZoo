10ded7d040cc44d26da5a634aac03c46213f2bc4
Nikolay <fomabb123@gmail.com>
2021-11-01 21:53:16 +0300

    add ArrayList animals

--- src/by/overone/lesson/Zoo.java
+++ src/by/overone/lesson/Zoo.java
@@ -1,7 +1,16 @@
 package by.overone.lesson;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zoo {
     public void zoo() {
+        List<String> animals = new ArrayList<>();
 
+        animals.add("Волк");
+        animals.add("Жераф");
+        animals.add("Енот");
+        animals.add("Носорог");
+        animals.add("Зубр");
     }
 }
