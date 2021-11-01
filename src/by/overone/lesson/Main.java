67ba783f4ee1769bdbefa2ea83420b109726d8e5
Nikolay <fomabb123@gmail.com>
2021-11-01 22:50:23 +0300

    trial run of the program

--- src/by/overone/lesson/Main.java
+++ src/by/overone/lesson/Main.java
@@ -9,5 +9,40 @@ public class Main {
 
         Zoo zoo = new Zoo();
         Wolf wolf = new Wolf();
+
+        System.out.println("ДОБРО ПОЖАЛОВАТЬ В НАШ ЗООПАРК!;)");
+        System.out.println("________________________________________________________");
+        zoo.zoo();
+        System.out.println();
+
+        wolf.name();
+        wolf.age();
+
+        System.out.println("Сегодня  можно покормить питомцев зоопарка\n");
+
+        while (true) {
+            System.out.println("1. Волк; \n2. Жераф; \n3. Енот; \n4. Носорог; \n5. Зубр;");
+            System.out.println("0. Окончить сеанс посещения зоопарка");
+
+            feed = scanner.nextInt();
+
+            if (feed >= 6) {
+                System.out.println("Будь внимательнее, такой команды нет ¯\\_(ツ)_/¯\n");
+                continue;
+            }
+
+            switch (feed) {
+                case 1:
+                    wolf.feed();
+                    continue;
+
+                case 0:
+                    System.out.println("Спасибо, что посетили нас, приходите еще;) Удачи Вам!!!");
+                    System.out.println("________________________________________________________");
+
+            }
+            break;
+        }
     }
 }
+
